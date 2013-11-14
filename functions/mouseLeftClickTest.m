85c83d0da46f2be6fd7dab832e90392ff2433758
Timo Rantalainen <tjrantal@gmail.com>
2013-11-14 14:11:05 +1100

    figuring out subplots in testCallbacks

--- functions/mouseLeftClickTest.m
+++ functions/mouseLeftClickTest.m
@@ -1,34 +1,28 @@
 function mouseLeftClickTest(objH,evt)
-	global initLineHandle overlayTrace sAx currentInit yLims xLims data epoch currentInit;
+	global initLineHandle overlayTrace sAx currentInit yLims xLims data epoch currentInit plotGeometry;
 		%Left click, set seedPoint
 		%Get the initIndex here
-		currentAxisIndex = find(sAx == get(objH,'Children'),1,'first');
 		
-        if exist('returnedPath','var')
-			%Set the initIndex here
-            %digitizedPath = cat(1,digitizedPath,returnedPath+1);
-        end
-        %set(gcf,'WindowButtonMotionFcn','');    %Set windowButtonMotion off for the duration of calculations
-        %set(gcf,'WindowButtonMotionFcn',@mouseMoved);   %Turn on windowButtonMotionFcn
-        point = get(get(objH,'Children'),'CurrentPoint');
-		testSize = size(point);
-		for i = 1:length(testSize)
-			disp(['dim ' num2str(i) ' size '  num2str(size(point))]);
+		figPosition = get(gcf,'position');
+		figCoordinate = get(gcf,'CurrentPoint');
+		for i = 1:2
+			relativePosition(i) = figCoordinate(i)/figPosition(i+2);
 		end
-        %seedPoint = [round(point(1,1)), round(point(1,2))]; 
-        %disp(['Seed ' num2str(seedPoint(:)')]);
-        %lineHandle = plot(seedPoint(1),seedPoint(2),'r-');
-        if ~exist('digitizedPath','var')
-            %digitizedPath(1,1) = seedPoint(2);
-            %digitizedPath(1,2) = seedPoint(1);
-        end
-        %drawnow;
+		%Figure out on which subplot the cursor must have been
+		row = ceil((1-relativePosition(1))*plotGeometry(1));
+		column = ceil(relativePosition(2)*plotGeometry(2));
+		currentAxisIndex = (row-1)*plotGeometry(2)+column;
+		
+
+			%currentAxisIndex = find(sAx == objH,1,'first');
+		
+        point = get(sAx(currentAxisIndex),'CurrentPoint');
 		set(initLineHandle(currentAxisIndex),'XData',[point(1) point(1)],'YData',yLims); %N.B. row, column and index differs by 1!!!
 		currentInit = max([1 find(data(1,:) >= point(1),1,'first')]);	%Cannot have init less than 1 
 		endEpoch = min([currentInit+epoch-1 size(data,2)]); %Prevent trying to plot out of bounds
 		plotEpoch = currentInit:endEpoch;
 		set(overlayTrace(currentAxisIndex),'XData',data(1,plotEpoch) ,'YData',data(2,plotEpoch)); %N.B. row, column and index differs by 1!!!
-		set(get(objH,'Children'),'xlim',xLims,'ylim',yLims);
+		set(sAx(currentAxisIndex),'xlim',xLims,'ylim',yLims);
 		%plot([point(1) point(1)],'YData',yLims,'g');
 		%plot(point(1),point(2),'r*');
         disp('Left Click');
--- testCallbacks.m
+++ testCallbacks.m
@@ -1,12 +1,13 @@
+%cd d:/timo/research/Reflex2013/analysis
 close all;
 clear all;
 clc;
 addpath('functions');   %Subfolder, which contains the analysis scripts for different conditions
 %Add global variables to be used by callback functions
-global initLineHandle overlayTrace sAx currentInit yLims xLims data epoch currentInit;
+global initLineHandle overlayTrace sAx currentInit yLims xLims data epoch currentInit plotGeometry;
 %callbacks are not implemented in gnuplot -> change plotting to fltk
 graphics_toolkit fltk;
-testF = figure('__graphics_toolkit__','fltk','position',[10,10,1000,1000]);
+testF = figure('__graphics_toolkit__','fltk','position',[10,10,1000,1000],'visible','off');
 
 t = linspace(0,1,100);
 sint = sin(2*pi*t);
@@ -17,17 +18,19 @@ tempLims = [min(data,[],2), max(data,[],2)];
 yLims = squeeze(tempLims(2,:));
 xLims = squeeze(tempLims(1,:));
 %test subplots with callbacks
+plotGeometry = [2,2];
 for i = 1:4
-	sAx(i) = subplot(2,2,i)
+	sAx(i) = subplot(2,2,i);
 	plot(data(1,:),data(2,:),'k.','linestyle','none');
 	hold on;
 	overlayTrace(i) = plot(data(1,currentInit:currentInit+epoch-1),data(2,currentInit:currentInit+epoch-1),'r');
 	initLineHandle(i) = plot([0.5 0.5],yLims);
+	%set(sAx(i),'WindowButtonUpFcn',@mouseLeftClickTest); 
 end
 
 
 
-
-set(gcf,'WindowButtonUpFcn',@mouseLeftClickTest);  %%LiveWire init and setting points are handled with callbacks
+set(testF,'visible','on');
+set(testF,'WindowButtonUpFcn',@mouseLeftClickTest);  %%LiveWire init and setting points are handled with callbacks
 %set(gcf,'WindowButtonMotionFcn',@mouseMoved); %Draw vertical line on the figure
 disp('Callback set');
